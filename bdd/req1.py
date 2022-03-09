2d1cdd1d4eec02eeb6b920226bf8eadfed407e8e
Rob Lemmens <C85953@launcher.int.abnamro.com>
2022-03-09 15:55:25 +0100

    corrected csv read

--- bdd/req1.py
+++ bdd/req1.py
@@ -4,11 +4,15 @@ from typing import List, Tuple
 from pyspark.sql import DataFrame, SparkSession
 from pyspark.sql.functions import col, lit, when
 
+dir_path = os.path.dirname(os.path.abspath(__file__))
+
+spark = SparkSession.builder.master("local").appName("CreateReq1").getOrCreate()
 
 def get_total_number_of_clients(client_details_path):
     
     spark = SparkSession.builder().getOrCreate()
-    df = spark.read.csv(client_details_path)
+    df = spark.read.option("header", True).option("inferSchema", True).csv(
+        dir_path + "/../fixtures/client_details.csv")
 
     return df.select("id").distinct().count()
 
